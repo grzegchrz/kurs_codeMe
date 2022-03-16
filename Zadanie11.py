c94ceb4bbf3c2c541012da47442bf0ffd3a8bdd2
Grzegorz Chrząstek <grzegorz.chrzastek@telemetria.eu>
2022-03-16 13:10:47 +0000

    Upload New File

---
+++ Zadanie11.py
@@ -0,0 +1,3 @@
+slowa_list = ['Grzegorz', 'Chrząstek', '39', 'Slupsk', 'Traugutta']
+
+print (f"Hello, nazywam się {slowa_list[0]} {slowa_list[1]} mam {slowa_list[2]} lat. Mieszkam w {slowa_list [3]} przy ulicy {slowa_list[4]}")
