16b4aa682547417d8dba156529ec5075b6bac0ac
Grzegorz Chrząstek <grzegorz.chrzastek@telemetria.eu>
2022-03-19 20:59:40 +0000

    Update Zadanie21.py Poprawiłem zmienne do określenia wartości min i max

--- Zadanie21.py
+++ Zadanie21.py
@@ -2,7 +2,14 @@ import pandas as pd
 
 df=pd.read_csv('przewoz_osob_gdynia.csv')
 
-q = df.max()
-p = df.min()
-print(f"Data wartości maksymalnej: {q[1]}")
-print(f"Data wartości minimalnej: {p[1]}")
+
+i = df['tys_osob'].idxmax()
+
+dateOfMax = (df['month'][i])
+
+j = df['tys_osob'].idxmin()
+
+dateOfMin = (df['month'][j])
+
+print(f"Data wartości maksymalnej: {dateOfMax}")
+print(f"Data wartości minimalnej: {dateOfMin}")
